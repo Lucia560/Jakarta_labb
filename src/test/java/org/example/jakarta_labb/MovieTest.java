a99d1747047e5b4227c9e8f44f1f577efe191136
Rishad Mahmudi <rismah1102@gmail.com>
2024-02-23 14:19:04 +0100

    Remove public from test cases

--- src/test/java/org/example/jakarta_labb/MovieTest.java
+++ src/test/java/org/example/jakarta_labb/MovieTest.java
@@ -6,35 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MovieTest {
 
     @Test
-    public void testIdGetterAndSetter() {
+    void testIdGetterAndSetter() {
         Movie movie = new Movie();
         movie.setId(1L);
         assertEquals(1L, movie.getId());
     }
 
     @Test
-    public void testTitleGetterAndSetter() {
+    void testTitleGetterAndSetter() {
         Movie movie = new Movie();
         movie.setTitle("Inception");
         assertEquals("Inception", movie.getTitle());
     }
 
     @Test
-    public void testReleaseYearGetterAndSetter() {
+    void testReleaseYearGetterAndSetter() {
         Movie movie = new Movie();
         movie.setReleaseYear(2010);
         assertEquals(2010, movie.getReleaseYear());
     }
 
     @Test
-    public void testGenreGetterAndSetter() {
+    void testGenreGetterAndSetter() {
         Movie movie = new Movie();
         movie.setGenre("Sci-Fi");
         assertEquals("Sci-Fi", movie.getGenre());
     }
 
     @Test
-    public void testRatingGetterAndSetter() {
+    void testRatingGetterAndSetter() {
         Movie movie = new Movie();
         movie.setRating(4.5);
         assertEquals(4.5, movie.getRating(), 0.001);
