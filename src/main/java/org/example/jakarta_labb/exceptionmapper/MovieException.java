0eaa744e8f00faa4c87a90412aa774728c287886
Rishad Mahmudi <rismah1102@gmail.com>
2024-02-29 22:24:19 +0100

    Added MovieException

---
+++ src/main/java/org/example/jakarta_labb/exceptionmapper/MovieException.java
@@ -0,0 +1,7 @@
+package org.example.jakarta_labb.exceptionmapper;
+
+public class MovieException extends RuntimeException {
+    public MovieException(String message) {
+        super(message);
+    }
+}
